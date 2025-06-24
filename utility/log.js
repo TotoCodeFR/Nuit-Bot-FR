1d241135097d0dcaa55679cd33bab8881a515a8a
TotoCode <totocodefr@duck.com>
2025-06-24 11:52:33 +0000

    fix (j'ai oublié .js sur un import)

--- utility/log.js
+++ utility/log.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { EmbedBuilder } from 'discord.js';
-import { loadConfig } from './server_config';
+import { loadConfig } from './server_config.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
